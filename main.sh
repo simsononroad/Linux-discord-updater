e3ed84ce542a47ad328ca26e171971a219189700
simsononroad <163458590+simsononroad@users.noreply.github.com>
2026-02-02 22:52:23 +0100

    Fix curl output path for Discord update

--- main.sh
+++ main.sh
@@ -3,5 +3,5 @@
 python3 main.py
 ver=$(cat version.txt)
 echo "$ver"
-curl https://stable.dl2.discordapp.net/apps/linux/$ver/discord-$ver.deb --output /home/dani/discord_update/rea/$ver.deb
+curl https://stable.dl2.discordapp.net/apps/linux/$ver/discord-$ver.deb --output rea/$ver.deb
 sudo dpkg -i rea/$ver.deb
